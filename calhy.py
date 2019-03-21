a7a3b97108922874476fd4d1a542b29699e86e0d
Wang Bin <binwang16@gmail.com>
2019-03-21 13:56:48 -0400

    add comments

--- calhy.py
+++ calhy.py
@@ -1,3 +1,15 @@
+"""
+double principle weekly
+total mortgage: b1 
+arm: b2
+frequency: b3
+interest: b4
+temp1 = b1*b4/(100*b3)+2*b1*b4/100*b3*b3*b2
+temp2 = 2*b1*b4/100*b2*b3*b3
+a(n) = temp1 - temp2 * n
+
+"""
+
 si = 0
 for i in range(1, 1300):
     ai = 116.74-0.19*i
